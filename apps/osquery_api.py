f7b4c806eb255110494c19ff714dcd3666a5f58a
srozb <github@rozbicki.eu>
2017-05-10 14:11:17 +0200

    event query fix

--- apps/osquery_api.py
+++ apps/osquery_api.py
@@ -6,7 +6,8 @@ from uuid import uuid4
 from bottle import Bottle, request, response, abort
 from . import db
 
-
+# TODO: node_key check decorator
+# TODO: check parameter escape
 app = Bottle()
 response.content_type = 'text/plain'
 
@@ -21,7 +22,8 @@ def enroll():  # TODO: autotag based on tag_rules
     def _create_node_key():
         return binascii.b2a_hex(os.urandom(16))
 
-    def _insert_new_client(node_key, hostname, bussiness_unit):  # TODO: check if already enrolled
+    # TODO: check if already enrolled
+    def _insert_new_client(node_key, hostname, bussiness_unit):
         osq_clients = db['osquery_client']
         osq_clients.insert(dict(hostname=hostname, uuid=str(uuid4()),
                                 node_key=node_key, bussiness_unit_id=bussiness_unit['id'],
@@ -52,19 +54,32 @@ def config():
         client_table = db['osquery_client']
         return client_table.find_one(node_key=node_key)
 
+    def _get_client_tags(client):
+        "return tag ids assigned to a given client"
+        tags_table = db['osquery_client_tag']
+        tag_id = []
+        for tag in tags_table.find(osqueryclient_id=client['id']):
+            tag_id.append(tag['id'])
+        return tag_id
+
     def _get_options(client):
         "get bussiness unit specific options"
         client_config_table = db['client_config']
-        options = client_config_table.find_one(bussiness_unit_id=client['bussiness_unit_id'])
+        options = client_config_table.find_one(
+            bussiness_unit_id=client['bussiness_unit_id'])
         if not options:
             options = client_config_table.find_one(name="default")
         return json.loads(options['template_config'])
 
-    def _get_event_quieries(client_id):
+    # TODO: make sure queries not duplicated if multiple tags assigned
+    def _get_event_quieries(tags):
         "get client specific quieries"
+        ids = []
+        for row in db['event_query_tag'].find(tag_id=tags):
+            ids.append(row['id'])
         event_queries = db['event_query'].find(
-            enabled='True')  # TODO: find based on tag
-        enabled_queries = {}
+            enabled=1, id=ids)  # TODO: test what if tag=None
+        enabled_queries = {}  # TODO: append untagged queries
         for query in event_queries:
             sql = {'query': str(query['value']),
                    'interval': str(query['interval'])}
@@ -74,16 +89,18 @@ def config():
     node_key = request.json['node_key']
     client = _get_client(node_key)
     print("config request from: {}".format(client['hostname']))
+    client_tags = _get_client_tags(client)
     options = _get_options(client)
-    schedule = _get_event_quieries(client['id'])
+    schedule = _get_event_quieries(client_tags)
     response_body = {'options': options}
-    if schedule:
+    if schedule:  # append to config only if not empty
         response_body['schedule'] = schedule
+    print response_body
     return response_body
 
 
 @app.route('/osquery/log', method='POST')
-def log_query_result():
+def log_query_result():  # TODO: send to redis
     "receive logs and query results from client"
     print("log received:")
     print(request.json)
